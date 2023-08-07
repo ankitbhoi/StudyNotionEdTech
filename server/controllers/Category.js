cef052e77aac448747e129c0a79773acb599dde4
ankitbhoi <ankitghosal2002@gmail.com>
2023-08-08 01:12:39 +0530

    updated

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -1,131 +1,117 @@
-const Category = require("../models/Category");
+const Category = require("../models/Category")
 
-//create Category ka handler funciton
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
+}
 exports.createCategory = async (req, res) => {
-	try {
-		//fetch data
-		const { name, description } = req.body;
-		//validation
-		if (!name) {
-			return res.status(400).json({
-				success: false,
-				message: 'All fields are required',
-			});
-		}
-		//create entry in DB
-		const CategoryDetails = await Category.create({
-			name: name,
-			description: description,
-		});
-		console.log(CategoryDetails);
-		//return response
-
-		return res.status(200).json({
-			success: true,
-			message: "Category Created Successfully",
-		});
-	}
-	catch (error) {
-		return res.status(500).json({
-			success: true,
-			message: error.message,
-		});
-	}
-};
-
-//getAllCategorys handler function
+  try {
+    const { name, description } = req.body
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" })
+    }
+    const CategorysDetails = await Category.create({
+      name: name,
+      description: description,
+    })
+    console.log(CategorysDetails)
+    return res.status(200).json({
+      success: true,
+      message: "Categorys Created Successfully",
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: true,
+      message: error.message,
+    })
+  }
+}
 
 exports.showAllCategories = async (req, res) => {
-	try {
-		const allCategorys = await Category.find({}, { name: true, description: true });
-		res.status(200).json({
-			success: true,
-			message: "All Categorys returned successfully",
-			data: allCategorys,
-		});
-	}
-	catch (error) {
-		return res.status(500).json({
-			success: false,
-			message: error.message,
-		});
-	}
-};
-
-//line 43->This line uses the Category model and the find() method to retrieve all Categorys from the database. The first argument {} specifies an empty filter, indicating that all Categorys should be returned. The second argument {name:true, description:true} is the projection parameter, which specifies that only the name and description fields should be included in the returned documents. This helps to reduce the amount of data transferred from the database. 
+  try {
+    const allCategorys = await Category.find()
+    res.status(200).json({
+      success: true,
+      data: allCategorys,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
 
 exports.categoryPageDetails = async (req, res) => {
-	try {
-		const { categoryId } = req.body;
+  try {
+    const { categoryId } = req.body
 
-		// Get courses for the specified category
-		const selectedCategory = await Category.findById(categoryId)
-			.populate("courses")
-			.exec();
-		console.log(selectedCategory);
-		// Handle the case when the category is not found
-		if (!selectedCategory) {
+    // Get courses for the specified category
+    const selectedCategory = await Category.findById(categoryId)
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+        populate: "ratingAndReviews",
+      })
+      .exec()
 
-			return res
-				.status(404)
-				.json({
-					success: false, message: "DATA not found",
-				});
-		}
-		// Handle the case when there are no courses
-		// if (selectedCategory.courses.length === 0) {
-		// 	console.log("No courses found for the selected category.");
-		// 	return res.status(404).json({
-		// 		success: false,
-		// 		message: "No courses found for the selected category.",
-		// 	});
-		// }
+    console.log("SELECTED COURSE", selectedCategory)
+    // Handle the case when the category is not found
+    if (!selectedCategory) {
+      console.log("Category not found.")
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" })
+    }
+    // Handle the case when there are no courses
+    if (selectedCategory.courses.length === 0) {
+      console.log("No courses found for the selected category.")
+      return res.status(404).json({
+        success: false,
+        message: "No courses found for the selected category.",
+      })
+    }
 
-		// const selectedCourses = selectedCategory.courses;
+    // Get courses for other categories
+    const categoriesExceptSelected = await Category.find({
+      _id: { $ne: categoryId },
+    })
+    let differentCategory = await Category.findOne(
+      categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+        ._id
+    )
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec()
+    console.log()
+    // Get top-selling courses across all categories
+    const allCategories = await Category.find()
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec()
+    const allCourses = allCategories.flatMap((category) => category.courses)
+    const mostSellingCourses = allCourses
+      .sort((a, b) => b.sold - a.sold)
+      .slice(0, 10)
 
-		// Get courses for other categories
-		const differentCategories = await Category.find({
-			_id: { $ne: categoryId },	//not equal to category ID
-		})
-		.populate("courses")
-		.exec();
-
-		// let differentCourses = [];
-		// for (const category of differentCategories) {
-		// 	differentCourses.push(...category.courses);
-		// }
-
-		// // Get top-selling courses across all categories
-		// const allCategories = await Category.find().populate("courses");
-		// const allCourses = allCategories.flatMap((category) => category.courses);
-		// const mostSellingCourses = allCourses
-		// 	.sort((a, b) => b.sold - a.sold)
-		// 	.slice(0, 10);
-
-		return res.status(200).json({
-			success:true,
-			data:{
-				selectedCategory,
-				differentCategories,
-				// mostSellingCourses
-			}
-		});
-	} catch (error) {
-		console.log(error);
-		return res.status(500).json({
-			success: false,
-			message: "Internal server error",
-			error: error.message,
-		});
-	}
-};
-
-
-// It fetches all the categories from the "Category" collection in the database and populates the "courses" field for each category using the .populate("courses") method. This allows you to retrieve the associated courses for each category.
-
-// The allCourses variable is then assigned the flattened array of courses obtained from the allCategories array. The flatMap method is used to iterate over each category and extract the "courses" array from it, creating a single flattened array of all courses.
-
-// Next, the mostSellingCourses array is generated by sorting the allCourses array in descending order based on the "sold" property of each course. The .sort((a, b) => b.sold - a.sold) function is used for this, with the comparison function sorting the courses based on their "sold" property.
-
-// Finally, the slice(0, 10) method is used to extract the first 10 elements from the sorted allCourses array, resulting in an array of the top 10 most selling courses.
+    res.status(200).json({
+      success: true,
+      data: {
+        selectedCategory,
+        differentCategory,
+        mostSellingCourses,
+      },
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    })
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,63 +1,64 @@
+// Importing necessary modules and packages
 const express = require("express");
 const app = express();
-
-const userRoutes = require("./routes/User");
-const profileRoutes = require("./routes/Profile");
-const paymentRoutes = require("./routes/Payments");
+const userRoutes = require("./routes/user");
+const profileRoutes = require("./routes/profile");
 const courseRoutes = require("./routes/Course");
-
+const paymentRoutes = require("./routes/Payments");
+const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const {cloudinaryConnect } = require("./config/cloudinary");
+const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
-dotenv.config();
+// Setting up port number
 const PORT = process.env.PORT || 4000;
 
-//database connect
+// Loading environment variables from .env file
+dotenv.config();
+
+// Connecting to database
 database.connect();
-//middlewares
+ 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
+		origin: "*",
+		credentials: true,
 	})
-)
-
-//The cors middleware is used to enable Cross-Origin Resource Sharing, allowing your application to handle requests from different origins (domains). The CORS middleware helps and define which origins are allowed to access your application's resources.
-// a. origin: This specifies the allowed origin(s) that can access your application's resources. In this case, requests originating from http://localhost:3000 will be allowed.
-// b. credentials: This indicates whether the server should include credentials (cookies, HTTP authentication, etc.) in the requests. By setting it to true, the server can respond with the appropriate Access-Control-Allow-Credentials header, allowing credentials to be included in the cross-origin requests.
-
+);
 app.use(
 	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
+		useTempFiles: true,
+		tempFileDir: "/tmp/",
 	})
-)
-//cloudinary connection
+);
+
+// Connecting to cloudinary
 cloudinaryConnect();
 
-//routes
+// Setting up routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/reach", contactUsRoute);
 
-
-//def route
-
+// Testing the server
 app.get("/", (req, res) => {
 	return res.json({
-		success:true,
-		message:'Your server is up and running....'
+		success: true,
+		message: "Your server is up and running ...",
 	});
 });
 
+// Listening to the server
 app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
+	console.log(`App is listening at ${PORT}`);
+});
 
+// End of code.
